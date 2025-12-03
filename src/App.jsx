51e5a8f8fb56430b0bd789485415ef3f96bac114
[Olatunbosun Victor] <[olatunbosunvictor1@gmail.com]>
2025-12-03 16:37:07 +0100

    updated dashboard, made a UX upgrade

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import SupportFlow from "./components/SupportFlow";
 import styles from "./App.module.css";
+import { Analytics } from "@vercel/analytics/next";
 
 export default function App() {
   return (
