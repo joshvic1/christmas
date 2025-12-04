679931eeda1e118d7abd94bcde8815ae5e1ae2cb
Olatunbosun Joshua <38948371+joshvic1@users.noreply.github.com>
2025-12-04 07:07:56 +0100

    Remove unused Analytics import
    Removed unused Analytics import from App component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import SupportFlow from "./components/SupportFlow";
 import styles from "./App.module.css";
-import { Analytics } from "@vercel/analytics/next";
 
 export default function App() {
   return (
